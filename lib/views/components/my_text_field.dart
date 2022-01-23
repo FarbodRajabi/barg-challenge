b00b9d6d50cc6488540b041d7980092185ccb6b7
Farbod <farbod.r.j.1@gmail.com>
2022-01-23 18:56:01 +0330

    text field widget created

---
+++ lib/views/components/my_text_field.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class MyTextField extends StatelessWidget {
+  final String hint;
+  final bool isPassword;
+  final TextEditingController controller;
+  final String? Function(String?) validator;
+
+  const MyTextField({
+    Key? key,
+    required this.hint,
+    required this.isPassword,
+    required this.controller,
+    required this.validator,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      style: TextStyle(
+        fontSize: 16,
+        color: Colors.green.shade800,
+        fontWeight: FontWeight.bold,
+        letterSpacing: 1.2,
+      ),
+      obscureText: false,
+      // controller: controller,
+      // validator: validator,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.zero,
+        hintText: hint,
+        hintStyle: const TextStyle(
+          color: Colors.grey,
+          fontWeight: FontWeight.normal,
+        ),
+        // errorBorder: const UnderlineInputBorder(
+        //   borderSide: BorderSide(
+        //     color: errorTextColor,
+        //     width: 2,
+        //   ),
+        // ),
+        // errorStyle: errorTextStyle,
+      ),
+    );
+  }
+}
--- lib/views/login_screen.dart
+++ lib/views/login_screen.dart
@@ -1,48 +1,91 @@
+import 'package:barg/views/components/my_text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+  LoginScreen({Key? key}) : super(key: key);
+
+  final formKey = GlobalKey<FormState>();
+
+  final TextEditingController userNameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        height: Get.height,
-        child: Stack(
-          children: [
-            Container(
-              height: Get.height * .4,
-              width: Get.width,
-              color: Colors.green.shade800,
-            ),
-            Positioned(
-              bottom: 0,
-              child: Container(
-                height: Get.height * .66,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: Get.height,
+          child: Stack(
+            children: [
+              Container(
+                height: Get.height * .4,
                 width: Get.width,
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(24),
+                color: Colors.green.shade800,
+              ),
+              Positioned(
+                bottom: 0,
+                child: Container(
+                  height: Get.height * .66,
+                  width: Get.width,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(24),
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.fromLTRB(0, 12, 0, 24),
+                        child: Form(
+                          key: formKey,
+                          child: Column(
+                            children: [
+                              Text(
+                                'Welcome back!',
+                                style: TextStyle(
+                                  color: Colors.green.shade800,
+                                  fontSize: 32,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'OpenSans',
+                                ),
+                              ),
+                              MyTextField(
+                                hint: 'Username',
+                                isPassword: false,
+                                controller: userNameController,
+                                validator: (value) {
+                                  if (value!.isEmpty || value.length < 4) {
+                                    return 'Provide a valid username';
+                                  } else {
+                                    return null;
+                                  }
+                                },
+                              ),
+                              MyTextField(
+                                hint: 'password',
+                                isPassword: false,
+                                controller: userNameController,
+                                validator: (value) {
+                                  if (value!.isEmpty || value.length < 4) {
+                                    return 'Provide a valid username';
+                                  } else {
+                                    return null;
+                                  }
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      // MyTextField(),
+                    ],
                   ),
                 ),
-                child: Column(
-                  children: [
-                    Text(
-                      'Welcome back!',
-                      style: TextStyle(
-                        color: Colors.green.shade800,
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'OpenSans',
-                      ),
-                    ),
-                  ],
-                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
